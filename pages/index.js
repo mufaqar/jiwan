8b735c3a650f9d68878cc9b4b34ec116367b539f
Mufaqar Islam <mufaqar@gmail.com>
2022-09-07 21:16:54 +0500

    index

--- pages/index.js
+++ pages/index.js
@@ -8,26 +8,28 @@ export default function Home() {
     <main>
       <Intro />
 
-      <section className='py-12 px-3'>
-        <div className='container mx-auto mb-10'>
-          <h3 className='text-sm'>CASE STUDY</h3>
+      <section className="py-12 px-3">
+        <div className="container mx-auto mb-10">
+          <h3 className="text-sm">CASE STUDY 01</h3>
         </div>
         <CaseStudy />
       </section>
 
-      <section className='py-12 px-3'>
-        <div className='container mx-auto mb-10'>
-          <h3 className='text-sm'>INSIGHTS</h3>
+      <section className="py-12 px-3">
+        <div className="container mx-auto mb-10">
+          <h3 className="text-sm">INSIGHTS</h3>
         </div>
-        <div className='container mx-auto items-center'>
-          <div className='grid md:grid-cols-2 grid-cols-1 gap-10'>
+        <div className="container mx-auto items-center">
+          <div className="grid md:grid-cols-2 grid-cols-1 gap-10">
             <InsightBox />
             <InsightBox />
             <InsightBox />
             <InsightBox />
           </div>
           <Link href="#">
-            <a className='md:text-[14px] text-[11px] text-[#1A4782] underline'>SEE MORE INSIGHTS</a>
+            <a className="md:text-[14px] text-[11px] text-[#1A4782] underline">
+              SEE MORE INSIGHTS
+            </a>
           </Link>
         </div>
       </section>
