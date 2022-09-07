4aae5e3a683e0dbf17ebea1bcdd3e041c3c28f8b
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2022-09-07 15:14:06 +0500

    header added

--- components/navbar.js
+++ components/navbar.js
@@ -12,34 +12,33 @@ export const Navbar = () => {
 
     return (
         <>
-            <nav className='grid grid-cols-3 p-3 '>
-                <div className={`${active ? '' : 'hidden'
-                        }   md:inline-flex md:flex-grow md:w-auto md:order-1`}
-                >
-                    <div className='md:inline-flex md:flex-row  w-full md:items-center items-start  flex flex-col md:h-auto'>
+            <nav className='flex md:flex-row flex-wrap items-center py-5 md:px-16 px-3'>
+                <div className={`${active ? ' w-full' : 'hidden'
+                    }  md:block md:order-first order-last md:w-2/12`}>
+                    <div className='inline-flex md:flex-row flex-col justify-between w-full text-xs leading-5'>
                         <Link href='/'>
-                            <a className='md:inline-flex md:w-auto w-full px-3 py-2 rounded font-bold items-center justify-center  '>
+                            <a className=''>
                                 CASE STUDIES
                             </a>
                         </Link>
                         <Link href='/'>
-                            <a className='md:inline-flex md:w-auto w-full px-3 py-2 rounded font-bold items-center justify-center '>
+                            <a className=' '>
                                 INSIGHTS
                             </a>
                         </Link>
                     </div>
                 </div>
                 <Link href='/'>
-                    <a className='order-2 inline-flex items-center p-2 mr-4 relative'>
-                       <Image src={Logo} className=""></Image>
+                    <a className='md:w-8/12 flex md:justify-center'>
+                        <Image src={Logo} className=""></Image>
                     </a>
                 </Link>
                 <button
-                    className=' inline-flex p-3 rounded md:hidden ml-auto outline-none'
+                    className='md:hidden ml-auto'
                     onClick={handleClick}
                 >
                     <svg
-                        className='w-6 h-6'
+                        className='w-8 h-10'
                         fill='none'
                         stroke='currentColor'
                         viewBox='0 0 24 24'
@@ -54,17 +53,16 @@ export const Navbar = () => {
                     </svg>
                 </button>
                 {/*Note that in this div we will use a ternary operator to decide whether or not to display the content of the div  */}
-                <div className={`${active ? '' : 'hidden'
-                        }   md:inline-flex md:flex-grow  order-3`}
-                >
-                    <div className='md:inline-flex md:flex-row md:ml-auto md:w-auto w-full md:items-center items-start  flex flex-col md:h-auto'>
+                <div className={`${active ? ' w-full' : 'hidden'
+                    }  md:block md:text-end order-last md:w-2/12`}>
+                    <div className='inline-flex md:flex-row flex-col justify-between w-full text-xs leading-5'>
                         <Link href='/'>
-                            <a className='md:inline-flex md:w-auto w-full px-3 py-2 rounded font-bold items-center justify-center '>
+                            <a className=' '>
                                 SERVICES
                             </a>
                         </Link>
                         <Link href='/'>
-                            <a className='md:inline-flex md:w-auto w-full px-3 py-2 rounded font-bold items-center justify-center '>
+                            <a className=' '>
                                 TEAM
                             </a>
                         </Link>
