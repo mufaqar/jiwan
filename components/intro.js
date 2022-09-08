4cf9eba67c0934c8653ba2680e6704404fc92a3b
Mufaqar Islam <mufaqar@gmail.com>
2022-09-08 20:05:00 +0500

    alt added

--- components/intro.js
+++ components/intro.js
@@ -1,29 +1,32 @@
-import Image from 'next/image'
+import Image from 'next/image';
 import IntroImg from '../public/images/introImg.png';
 import MblIntro from '../public/images/mblintro.png';
 
 export default function Intro() {
-    return (
-        <>
-            <section className='py-14 md:pt-14 pt-0'>
-                <div className='container mx-auto grid md:grid-cols-2 grid-cols-1 md:gap-0 gap-9 items-center'>
-                    <div className='md:order-first order-last md:px-0 px-4'>
-                        <h2 className='lg:text-[64px] md:leading-[64px] md:text-[54px] text-[36px] leading-[36px] font-ITCGaramondStdLight tracking-tighter capitalize'>What does your business <br></br><span className='italic'>buy you?</span></h2>
-                    </div>
-                    <div>
-                        <figure className="md:flex hidden md:justify-end">
-                            <Image src={IntroImg}></Image>
-                        </figure>
-                        <figure className="md:hidden flex w-full h-[624px] relative">
-                            <Image src={MblIntro} layout='fill'></Image>
-                        </figure>
-                        <div className='text-xs max-w-[539px] ml-auto py-3 md:px-0 px-3'>
-                            <p>Mah Ji preparing sugar canes for Sukhanoop Singh Dhillon.</p>
-                            <p>Kanjli, Dist. Sangrur, 1989.</p>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </>
-    )
+  return (
+    <>
+      <section className="py-14 md:pt-14 pt-0">
+        <div className="container mx-auto grid md:grid-cols-2 grid-cols-1 md:gap-0 gap-9 items-center">
+          <div className="md:order-first order-last md:px-0 px-4">
+            <h2 className="lg:text-[64px] md:leading-[64px] md:text-[54px] text-[36px] leading-[36px] font-ITCGaramondStdLight tracking-tighter capitalize">
+              What does your business <br></br>
+              <span className="italic">buy you?</span>
+            </h2>
+          </div>
+          <div>
+            <figure className="md:flex hidden md:justify-end">
+              <Image src={IntroImg} alt=""></Image>
+            </figure>
+            <figure className="md:hidden flex w-full h-[624px] relative">
+              <Image src={MblIntro} layout="fill" alt=""></Image>
+            </figure>
+            <div className="text-xs max-w-[539px] ml-auto py-3 md:px-0 px-3">
+              <p>Mah Ji preparing sugar canes for Sukhanoop Singh Dhillon.</p>
+              <p>Kanjli, Dist. Sangrur, 1989.</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
--- components/navbar.js
+++ components/navbar.js
@@ -4,71 +4,66 @@ import { useState } from 'react';
 import Logo from '../public/images/logo.png';
 
 export const Navbar = () => {
-    const [active, setActive] = useState(false);
+  const [active, setActive] = useState(false);
 
-    const handleClick = () => {
-        setActive(!active);
-    };
+  const handleClick = () => {
+    setActive(!active);
+  };
 
-    return (
-        <>
-            <nav className='flex md:flex-row flex-wrap items-center py-8 md:px-16 px-3'>
-                <div className={`${active ? ' w-full' : 'hidden'
-                    }  md:block md:order-first order-last md:w-2/12`}>
-                    <div className='inline-flex md:flex-row flex-col justify-between w-full text-xs leading-5'>
-                        <Link href='/'>
-                            <a className=''>
-                                CASE STUDIES
-                            </a>
-                        </Link>
-                        <Link href='/'>
-                            <a className=' '>
-                                INSIGHTS
-                            </a>
-                        </Link>
-                    </div>
-                </div>
-                <Link href='/'>
-                    <a className='md:w-8/12 flex md:justify-center'>
-                        <Image src={Logo} className=""></Image>
-                    </a>
-                </Link>
-                <button
-                    className='md:hidden ml-auto'
-                    onClick={handleClick}
-                >
-                    <svg
-                        className='w-8 h-10'
-                        fill='none'
-                        stroke='currentColor'
-                        viewBox='0 0 24 24'
-                        xmlns='http://www.w3.org/2000/svg'
-                    >
-                        <path
-                            strokeLinecap='round'
-                            strokeLinejoin='round'
-                            strokeWidth={2}
-                            d='M4 6h16M4 12h16M4 18h16'
-                        />
-                    </svg>
-                </button>
-                {/*Note that in this div we will use a ternary operator to decide whether or not to display the content of the div  */}
-                <div className={`${active ? ' w-full' : 'hidden'
-                    }  md:block md:text-end order-last md:w-2/12`}>
-                    <div className='inline-flex md:flex-row flex-col justify-between w-full text-xs leading-5'>
-                        <Link href='/'>
-                            <a className=' '>
-                                SERVICES
-                            </a>
-                        </Link>
-                        <Link href='/'>
-                            <a className=' '>
-                                TEAM
-                            </a>
-                        </Link>
-                    </div>
-                </div>
-            </nav>
-        </>
-    );
+  return (
+    <>
+      <nav className="flex md:flex-row flex-wrap items-center py-8 md:px-16 px-3">
+        <div
+          className={`${
+            active ? ' w-full' : 'hidden'
+          }  md:block md:order-first order-last md:w-2/12`}
+        >
+          <div className="inline-flex md:flex-row flex-col justify-between w-full text-xs leading-5">
+            <Link href="/">
+              <a className="">CASE STUDIES</a>
+            </Link>
+            <Link href="/">
+              <a className=" ">INSIGHTS</a>
+            </Link>
+          </div>
+        </div>
+        <Link href="/">
+          <a className="md:w-8/12 flex md:justify-center">
+            <Image src={Logo} className="" alt=""></Image>
+          </a>
+        </Link>
+        <button className="md:hidden ml-auto" onClick={handleClick}>
+          <svg
+            className="w-8 h-10"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M4 6h16M4 12h16M4 18h16"
+            />
+          </svg>
+        </button>
+        {/*Note that in this div we will use a ternary operator to decide whether or not to display the content of the div  */}
+        <div
+          className={`${
+            active ? ' w-full' : 'hidden'
+          }  md:block md:text-end order-last md:w-2/12`}
+        >
+          <div className="inline-flex md:flex-row flex-col justify-between w-full text-xs leading-5">
+            <Link href="/">
+              <a className=" ">SERVICES</a>
+            </Link>
+            <Link href="/">
+              <a className=" ">TEAM</a>
+            </Link>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 };
