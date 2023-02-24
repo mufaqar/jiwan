22523ec37d33962ba1267ed4cb3da2e2b2007ce4
Mufaqar Islam <mufaqar@gmail.com>
2023-02-24 20:33:47 +0500

    svg icons

--- components/partners.js
+++ components/partners.js
@@ -1,23 +1,23 @@
-import Image from 'next/image'
-import Partner1 from '../public/images/partner1.png';
-import Partner2 from '../public/images/partner2.png';
+import Image from 'next/image';
+import Partner1 from '../public/images/cpa.svg';
+import Partner2 from '../public/images/ctf.svg';
 
 export default function Partners() {
-    return (
-        <>
-            <section className="py-10 px-4 ">
-                <div className="container mx-auto">
-                    <h3 className="text-sm mb-10">PROFFESIONAL ASSOCIATIONS</h3>
-                    <div className='grid grid-cols-2 gap-5 md:max-w-[898px] mx-auto'>
-                        <figure>
-                            <Image src={Partner1} alt=''></Image>
-                        </figure>
-                        <figure>
-                            <Image src={Partner2} alt=''></Image>
-                        </figure>
-                    </div>
-                </div>
-            </section>
-        </>
-    )
+  return (
+    <>
+      <section className="py-10 px-4 ">
+        <div className="container mx-auto">
+          <h3 className="text-sm mb-10">PROFFESIONAL ASSOCIATIONS</h3>
+          <div className="grid grid-cols-2 gap-5 md:max-w-[898px] mx-auto">
+            <figure>
+              <Image src={Partner1} alt=""></Image>
+            </figure>
+            <figure>
+              <Image src={Partner2} alt=""></Image>
+            </figure>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
