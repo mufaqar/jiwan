8b3ab898a7e74b16063166d41c81bf6ba697f3ea
Mufaqar Islam <mufaqar@gmail.com>
2023-02-28 11:29:55 +0500

    upated

--- components/case-study.js
+++ components/case-study.js
@@ -5,20 +5,18 @@ const settings = {
   dots: true,
   infinite: true,
   autoplay: true,
-  speed: 500,
+  speed: 800,
   slidesToShow: 1,
   slidesToScroll: 1,
-  useCSS: false,
-  useTransform: false,
 };
 
 const SimpleSlider = ({ casestudy }) => {
   return (
     <>
-      <div className="container mx-auto">
+      <div className="container mx-auto ">
         <Slider {...settings}>
           {casestudy?.map((slide, index) => (
-            <div key={index} className="container mx-auto">
+            <div key={index} className="container mx-auto p-4 ">
               <div className="grid border border-black/70 md:py-10 md:px-14 md:gap-14">
                 <div className="grid grid-cols-1 gap-5 px-8 py-5 md:grid-cols-2 md:p-0">
                   <h2 className="case_slider_heading">{slide?.title}</h2>
@@ -37,14 +35,14 @@ const SimpleSlider = ({ casestudy }) => {
                       <h2 className="font-ITCGaramondLightCondensed md:text-[42px] md:leading-[50px] text-[34px] tracking-tighter">
                         {profit?.amountAndPersontage}
                       </h2>
-                      <p className="md:text-[13px] text-[9px] text-[#1A4782] max-w-[241px] mx-auto">
+                      <p className="md:text-[13px] text-[9px] text-[#1A4782] max-w-[241px] mx-auto min-h-[56px]">
                         {profit?.info}
                       </p>
                     </div>
                   ))}
                 </div>
 
-                <div className="max-w-[900px] md:p-0 py-5 px-8">
+                <div className="max-w-[1000px] md:p-0 py-5 px-8">
                   <div
                     className="case_study_details"
                     dangerouslySetInnerHTML={{
